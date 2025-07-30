3fd66307fb96a5ce01503e2e26a20026b5244410
JoyoMDEV <joyo99@gmx.de>
2025-07-30 11:59:19 +0200

    needed to add JsonProperty to use the same run skript for java and python microservices

--- java-spring-boot/shop/src/main/java/com/example/shop/dto/ShopPurchaseRequest.java
+++ java-spring-boot/shop/src/main/java/com/example/shop/dto/ShopPurchaseRequest.java
@@ -1,10 +1,13 @@
 package com.example.shop.dto;
 
 import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonProperty;;
 
 @Data
 public class ShopPurchaseRequest {
+    @JsonProperty("product_id")
     private Long productId;
     private Integer quantity;
+    @JsonProperty("payment_method")
     private String paymentMethod = "credit_card";
 }
